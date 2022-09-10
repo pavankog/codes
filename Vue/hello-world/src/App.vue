d123ae310a3506f1cee6c26fd522bc3ecaa72a35
shivaraju <Shivaraju.Mahadev@marlabs.com>
2022-09-10 23:16:16 +0530

    Code Clean up

--- Vue/hello-world/src/App.vue
+++ Vue/hello-world/src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <Form />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Form from './Forms/form.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Form
+}
 }
 </script>
 
---
+++ Vue/hello-world/src/Forms/form.vue
@@ -0,0 +1,48 @@
+<template>
+    <form>
+        <input v-model="employee.name" type="text" />
+        <input type="radio" v-model="employee.chooseColor" value='red' />red
+        <input type="radio" v-model="employee.chooseColor" value='blue' />blue
+        <input type="radio" v-model="employee.chooseColor" value='green' />green
+        <div :style="
+        [{'color':employee.chooseColor}, this.employee.BackGroudcolorGroup]"
+            :class="['fontClass',{changeFont:this.employee.addClass}, ]">
+            color
+        </div>
+        <button type="button" @click="addFonts">add font size</button>
+
+    </form>
+</template>
+<script>
+export default {
+    name: 'formComponent',
+    data() {
+        return {
+            employee: { name: 'test', chooseColor: 'black', BackGroudcolorGroup: {}, addClass: false }
+        }
+    },
+    methods: {
+        changeWidth(size) {
+            this.employee.addClass = true;
+        },
+        addFonts() {
+
+            this.employee.addClass = true;
+        },
+        addBackGroundColor() {
+            this.employee.BackGroudcolorGroup = { 'background-color': 'green' }
+        }
+    }
+}
+
+</script>
+<style>
+.fontClass {
+    font-size: 30px;
+    color: 'black'
+}
+
+.changeFont {
+    font-size: 70px;
+}
+</style>
