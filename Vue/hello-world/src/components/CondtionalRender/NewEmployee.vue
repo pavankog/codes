d60976b35b4ebd69d4b96a209674e9b052acdd94
Your Nameshivu <you@example.com>
2022-09-11 10:41:12 +0530

    code clean up

---
+++ Vue/hello-world/src/components/CondtionalRender/NewEmployee.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div v-for="employee in Employees" :key="employee.name" style="cursor:pointer">
+            <li @click="SelectedEmployee=employee"> {{employee.name}}</li>
+        </div>
+        <div v-if="SelectedEmployee">
+            <input type="text" v-model="SelectedEmployee.name" />
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NewEmployee',
+    data() {
+        return {
+            SelectedEmployee: undefined,
+            Employees: [
+                {
+                    name: 'pavan',
+                    address: { area: 'testarea', street: 'teststreet' }
+                }, {
+                    name: 'manju',
+                    address: { area: 'testarea', street: 'teststreet' }
+                }
+            ]
+        }
+    }
+}
+</script>
+<style>
+
+</style>
